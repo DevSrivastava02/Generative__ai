e00d1f49da48850e39e2a78acc25c2e79fc7288d
DevSrivastava02 <Srivastavaharsh427@gmail.com>
2025-04-29 01:12:35 +0530

    Introduction of Numpy

---
+++ Weak2_day1.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr=np.array([1,2,3,4,5])
+print(arr)
+#print the 3,2, matrix of zeroes
+zeroes=np.zeros([3,2])
+print(zeroes)
+#ones of matrix 5,3
+ones=np.ones([5,3])
+print(ones)
