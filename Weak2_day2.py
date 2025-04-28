4eda5c2b9c6f3d2c3d8fd91989ad15a2c38c6d41
DevSrivastava02 <Srivastavaharsh427@gmail.com>
2025-04-29 01:30:56 +0530

    Basic opertion on array

---
+++ Weak2_day2.py
@@ -0,0 +1,21 @@
+# Basic opertion on Array
+import numpy as np
+arr=np.array([1,2,3,4,5,6,7,8,9])
+print(arr)
+arr1=np.array([9,8,7,6,5,4,3,2,1])
+print(arr1)
+#Element_Wisw opertion
+print(arr+arr1)
+print(arr-arr1)
+print(arr*arr1)
+print(arr//arr1)
+print(arr/arr1)
+
+# Mathematical opertaion
+
+print(np.max(arr))
+print(np,min(arr))
+print(np.sqrt(arr))
+print(np.mean(arr))
+
+
