92551af9d3e0a018dbda44a8fbeccfe74bb820e9
DevSrivastava02 <Srivastavaharsh427@gmail.com>
2025-04-29 01:22:35 +0530

    Use Dimension/Manipulation

--- Weak2_day1.py
+++ Weak2_day1.py
@@ -1,7 +1,18 @@
 import numpy as np
-arr=np.array([1,2,3,4,5])
+arr=np.array([1,2,3,4,5,6])
 print(arr)
-#print the 3,2, matrix of zeroes
+
+
+#Daimension/manipulating
+
+reshaped=arr.reshape((2,3))
+print(reshaped)
+
+
+#so instead of a  unit nedd one single dimensions,it's going to have two by thrice matix for me
+expended=arr[:,np.newaxis]
+print(expended)
+# print the 3,2, matrix of zeroes
 zeroes=np.zeros([3,2])
 print(zeroes)
 #ones of matrix 5,3
