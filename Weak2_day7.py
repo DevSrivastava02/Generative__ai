b3c6df0829eb5a3f0ba5752a95ba7e0914073dc0
DevSrivastava02 <Srivastavaharsh427@gmail.com>
2025-04-30 01:28:01 +0530

    Basic Histrogram chart

--- Weak2_day7.py
+++ Weak2_day7.py
@@ -19,10 +19,20 @@ import matplotlib.pyplot as plt
 # plt.show()
 
 #Bar chart
-Categories=["A","B","C","D"]
-values=[12,34,56,23]
-plt.bar(Categories,values,color="skyblue")
-plt.title("Bar chart")
+# Categories=["A","B","C","D"]
+# values=[12,34,56,23]
+# plt.bar(Categories,values,color="skyblue")
+# plt.title("Bar chart")
+# plt.xlabel("X_label")
+# plt.ylabel("Y_label")
+# plt.show()
+
+
+#Histrogram chart
+Data=[1,2,2,3,3,3,4,4,4,4]
+plt.hist(Data,bins=4,color="skyblue",edgecolor="black",label="trends")
+plt.title("Histrogram chart")
 plt.xlabel("X_label")
 plt.ylabel("Y_label")
+plt.legend()
 plt.show()
