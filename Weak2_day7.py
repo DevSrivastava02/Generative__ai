0be00a907ba4b909374d252faa68a71356ebda74
DevSrivastava02 <Srivastavaharsh427@gmail.com>
2025-04-30 01:39:32 +0530

    Customizing plots

--- Weak2_day7.py
+++ Weak2_day7.py
@@ -2,40 +2,40 @@
 # it allow for detailed customerization of ploats
 
 import matplotlib.pyplot as plt
-# x=[1,2,3,4,5]
-# y=[7,8,9,0,7]
-# plt.plot(x,y)
-# plt.show()
+x=[1,2,3,4,5]
+y=[7,8,9,0,7]
+plt.plot(x,y)
+plt.show()
 
 # Line Ploats
 
-# x=[1,2,3,4,5]
-# y=[6,7,8,9,0]
-# plt.plot(x,y ,label="trend")
-# plt.title("Line Plot")
-# plt.xlabel("X_label")
-# plt.ylabel("Y_label")
-# plt.legend()
-# plt.show()
+x=[1,2,3,4,5]
+y=[6,7,8,9,0]
+plt.plot(x,y ,label="trend")
+plt.title("Line Plot")
+plt.xlabel("X_label")
+plt.ylabel("Y_label")
+plt.legend()
+plt.show()
 
 #Bar chart
-# Categories=["A","B","C","D"]
-# values=[12,34,56,23]
-# plt.bar(Categories,values,color="skyblue")
-# plt.title("Bar chart")
-# plt.xlabel("X_label")
-# plt.ylabel("Y_label")
-# plt.show()
+Categories=["A","B","C","D"]
+values=[12,34,56,23]
+plt.bar(Categories,values,color="skyblue")
+plt.title("Bar chart")
+plt.xlabel("X_label")
+plt.ylabel("Y_label")
+plt.show()
 
 
 #Histrogram chart
-# Data=[1,2,2,3,3,3,4,4,4,4]
-# plt.hist(Data,bins=4,color="skyblue",edgecolor="black",label="trends")
-# plt.title("Histrogram chart")
-# plt.xlabel("X_label")
-# plt.ylabel("Y_label")
-# plt.legend()
-# plt.show()
+Data=[1,2,2,3,3,3,4,4,4,4]
+plt.hist(Data,bins=4,color="skyblue",edgecolor="black",label="trends")
+plt.title("Histrogram chart")
+plt.xlabel("X_label")
+plt.ylabel("Y_label")
+plt.legend()
+plt.show()
 
 
 #Scattor plot
@@ -47,3 +47,12 @@ plt.ylabel("Y_label")
 plt.legend()
 plt.show()
 
+#Customizing plot
+x=[1,2,3,4,5]
+y=[23,45,67,34,56]
+plt.plot(x,y,label="trends",color="red",linestyle="--", marker="o")
+plt.xlabel("X_label")
+plt.ylabel("Y_label")
+plt.legend()
+plt.show()
+ 
