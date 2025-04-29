d92f7268ea8318cda51630d8358413ffc53dd519
DevSrivastava02 <Srivastavaharsh427@gmail.com>
2025-04-29 23:47:03 +0530

    Loading data for csv and excel

--- Weak2_day5.py
+++ Weak2_day5.py
@@ -9,3 +9,17 @@ data={"name":["dev","ayush","lala","jai"],
       "Age":[12,3,45,6]}
 df=pd.DataFrame(data)
 print(df)
+
+
+# Loading data from csv,excel,and other
+
+# import pandas as pd
+# df=pd.read_csv("data.csv")
+# df=pd.read_excel("data.text")
+# df=pd.DataFrame(data)
+
+
+# #to save the Data
+# df.to_csv("data.csv")
+# df.to_excel("data.text" ,index=False)
+
