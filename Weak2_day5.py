474d222c9abb232bcb6a76046ad6f14e6590d53a
DevSrivastava02 <Srivastavaharsh427@gmail.com>
2025-04-30 00:12:03 +0530

    Basic data frame operation

--- Weak2_day5.py
+++ Weak2_day5.py
@@ -1,25 +1,26 @@
-# introduction of pandas..
-# series and DataFrame..
-
 import pandas as pd
-s=pd.Series([10,20,30],index=["a","b","c"])
-print(s)
+import numpy as np
 
-data={"name":["dev","ayush","lala","jai"],
-      "Age":[12,3,45,6]}
+#upload the raw data
+data=pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv")
 df=pd.DataFrame(data)
-print(df)
-
-
-# Loading data from csv,excel,and other
-
-# import pandas as pd
-# df=pd.read_csv("data.csv")
-# df=pd.read_excel("data.text")
-# df=pd.DataFrame(data)
-
-
-# #to save the Data
-# df.to_csv("data.csv")
-# df.to_excel("data.text" ,index=False)
-
+#read the first 5 line
+print(df.head())
+#read the last  5 line
+print(df.tail())
+#read the info of the data
+print(df.info())
+# read the mean max min etc
+print(df.describe())
+#read the element by selecting the indexing
+print(df["sepal_width"])
+#read the multiple indexing
+print(df[["sepal_width","petal_length"]])
+#filtue the raw data
+print(df[df["petal_length"]>3])
+#selcting by position
+print(df.iloc[0]) # for row [0]
+print(df.iloc[:,0]) # for coloum[:,0]
+#selecing by label
+print(df.loc[0])  # it will give u the first row by indexing
+print(df.loc[:"petal_length"])
