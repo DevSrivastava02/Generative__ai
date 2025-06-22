cd4ebb7f13bd09bc6ab941cf86af8686f00fb188
DevSrivastava02 <Srivastavaharsh427@gmail.com>
2025-06-22 22:04:36 +0530

    plot and explore all the distribution probibility

--- Weak3_day5.py
+++ Weak3_day5.py
@@ -15,17 +15,53 @@ import matplotlib.pyplot as plt
 # plt.title("Guassien Distribution")
 # plt.show()
 
-def basyes_theorm(prior,sensitivity,specification):
-    evidence=(sensitivity*prior)+((1-specification)*(1-prior))
-    posterior=(sensitivity*prior)/evidence
-    return posterior
+# a disease affects 1% of the population
+# a test is 95% accurate for disease individual and 90% accurate for non-disease individual
+# find the probability of having the disease given a positive test result
 
-prior=0.01
-sensitivity=0.95
-specification=0.90
+# def basyes_theorm(prior,sensitivity,specification):
+#     evidence=(sensitivity*prior)+((1-specification)*(1-prior))
+#     posterior=(sensitivity*prior)/evidence
+#     return posterior
 
-posterior=basyes_theorm(prior,sensitivity,specification)
-print("porobility of disesas given is:",posterior)
+# prior=0.01
+# sensitivity=0.95
+# specification=0.90
+
+# posterior=basyes_theorm(prior,sensitivity,specification)
+# print("porobility of disesas given is:",posterior)
+
+# plot and explore differnent probability Distribution
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm,binom,poisson
+
+# Gaussian distribution
+# x=np.linspace(-4,4,100)
+# y=norm.pdf(x,loc=0,scale=1)
+# plt.plot(x,y,label="Gaussian")
+# plt.title("Gaussian distribution")
+# plt.show()
+
+# binomial distribution
+# n,p=10,0.5
+# x=np.arange(0,n+1)
+# y=binom.pmf(x,n,p)
+# plt.bar(x,y, label="bonomial")
+# plt.title("Binomal distribution")
+# plt.show()
+
+# poisson distribution
+
+lam=3
+x=np.arange(0,10)
+y=poisson.pmf(x,lam)
+plt.bar(x,y,label="Poisson")
+plt.title("Poisson distribution")
+plt.show()
+
+
+
 
 
 
