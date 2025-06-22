a7d2b9c7adada9436506cadfcc4b1ce43a1d1ed2
DevSrivastava02 <Srivastavaharsh427@gmail.com>
2025-06-22 11:24:54 +0530

    Gradient descent function (important)

---
+++ Weak3_day3.py
@@ -0,0 +1,18 @@
+import numpy as np
+# define the gradient descent function
+def Optimal_Descent(X,Y,theta,learning_rate,iteration):
+    m=len(Y)
+    for _ in range(iteration):
+        prediction=np.dot(X,theta)
+        errors=prediction-Y
+        gradient=(1/m)*np.dot(X.T,errors)
+        theta-=learning_rate*gradient
+    return theta   
+X=np.array([[1,1],[1,2],[3,4]])
+Y=np.array([1,0,1])
+theta=np.array([0.1,0.1])
+learning_rate=0.1
+iteration=1000
+Optimal_theta=Optimal_Descent(X,Y,theta,learning_rate,iteration)
+print("Optimal descent\n",Optimal_theta)
+ 
