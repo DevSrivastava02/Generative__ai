3696dace70572ee07629d38e5427e79b86a4ea58
DevSrivastava02 <Srivastavaharsh427@gmail.com>
2025-06-24 22:50:21 +0530

    # Create and Analyze Random variable

--- Weak4_day1.py
+++ Weak4_day1.py
@@ -1,12 +1,19 @@
-# Simulate Dice Roll and Calculate Probility
-
-# Simulate 10000 rice roll together
+# Create and Analyze Random variable
 import numpy as np
+import matplotlib.pyplot as mlt
+from scipy.stats import uniform
+# Descrite Random Variable
+Rice_rool=[1,2,3,4,5,6]
+Probility=np.sum([1/6]*6)
 
-Rool_dice=np.random.randint(1,7,size=10000)
+mlt.bar(Rice_rool,Probility,color="green",alpha=0.5)
+mlt.title("Descrite Random Variable")
+mlt.show()
 
-Even_prob=np.sum(Rool_dice%2==0)/len(Rool_dice)
-Pro_of_Greaterthen4=np.sum(Rool_dice>4)/len(Rool_dice)
+# Contineus Random Variable
 
-print("Even probility is:",Even_prob)
-print("The probility which greater then 4 is:",Pro_of_Greaterthen4)
+Rice_rool1=np.linspace(0,1,100)
+Probility1=uniform.pdf(Rice_rool1,loc=0,scale=1)
+mlt.plot(Rice_rool1,Probility1,color="red")
+mlt.title("Contineus Random Variable")
+mlt.show()
