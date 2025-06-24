6735434d4961e02c07802b41eafc3ef8da5c80fa
DevSrivastava02 <Srivastavaharsh427@gmail.com>
2025-06-24 20:31:25 +0530

    Basic probablity

---
+++ Weak4_day1.py
@@ -0,0 +1,13 @@
+# Probability theory and random variable
+
+# Basic probablity
+# Import liberary
+
+
+
+from itertools import product
+Sample_value=list(range(1,7))
+even=[2,4,6]
+
+Even_prob=len(even)/len(Sample_value)
+print("The probility of even outcome is:",Even_prob)
