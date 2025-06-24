449a39930c906d7c05385ee85df1308cb87de711
DevSrivastava02 <Srivastavaharsh427@gmail.com>
2025-06-24 21:08:57 +0530

    Simulate Dice Roll and Calculate Probility

--- Weak4_day1.py
+++ Weak4_day1.py
@@ -1,31 +1,12 @@
-# Probability theory and random variable
+# Simulate Dice Roll and Calculate Probility
 
-# Basic probablity
-# Import liberary
-# *********************************************
-# from itertools import product
-# Sample_value=list(range(1,7))
-# even=[2,4,6]
-
-# Even_prob=len(even)/len(Sample_value)
-# print("The probility of even outcome is:",Even_prob)
-
-#******************************************************
-# Random variable
+# Simulate 10000 rice roll together
 import numpy as np
 
-Outcome=np.array([1,2,3,4,5,6])
-Probility=np.array([1/6]*6)
+Rool_dice=np.random.randint(1,7,size=10000)
 
-# Expection E[x]
+Even_prob=np.sum(Rool_dice%2==0)/len(Rool_dice)
+Pro_of_Greaterthen4=np.sum(Rool_dice>4)/len(Rool_dice)
 
-Expection1=np.sum(Outcome*Probility)
-print("Expextion(mean):",Expection1)
-
-# Varienace V[x]
-Variance=np.sum((Outcome-Expection1)**2 *Probility)
-print("Varienace is:",Variance)
-
-# Standard Derivation
-Std=np.sqrt(Variance)
-print("Standard derivation:",Std)
+print("Even probility is:",Even_prob)
+print("The probility which greater then 4 is:",Pro_of_Greaterthen4)
